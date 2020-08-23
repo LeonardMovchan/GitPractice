cc6eb9707a84a1a835eeb554bc277654998395e7
Leonard Movchan <lanakrit@gmail.com>
2020-08-23 14:25:40 +0300

    Some changes

--- GitPractice/Program.cs
+++ GitPractice/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,45 @@ namespace GitPractice
     {
         static void Main(string[] args)
         {
+            do
+            {
+                Console.WriteLine("This is A Menu");
+                Console.WriteLine("1. Add new game");
+                Console.WriteLine("2. Check all avaialble games ");
+                Console.WriteLine("3. Remove Game by Index");
+                Console.WriteLine("4. Exit >>");
 
-        
+                Menu comand = (Menu)int.Parse(Console.ReadLine());
+
+                switch (comand)
+                {
+                    case Menu.AddNewGame:
+                        {
+
+                        }
+                        break;
+                    case Menu.CheckGames:
+                        {
+
+                        }
+                        break;
+                    case Menu.RemoveGames:
+                        {
+
+                        }
+                        break;
+                    case Menu.Exit:
+                        {
+
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("There is no such index");
+                        }
+                        break;
+                }
+            } while (true);
         }
 
         enum Menu
